62654647d03102c93b49bec35d490f44226d7401
Steven Hepting <shepting@gmail.com>
2021-05-02 23:25:13 -0600

    Set group.

--- main.ts
+++ main.ts
@@ -21,12 +21,17 @@ input.onButtonPressed(Button.AB, function () {
     music.playTone(330, music.beat(BeatFraction.Double))
     RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 360)
 })
+radio.onReceivedString(function (receivedString) {
+    music.playTone(262, music.beat(BeatFraction.Eighth))
+    basic.showString(receivedString)
+})
 input.onButtonPressed(Button.B, function () {
     led.plot(3, 3)
     music.playTone(196, music.beat(BeatFraction.Whole))
     RingbitCar.running_time(RingbitCar.Direction_run.backward, 3)
 })
 let lightsOn = false
+radio.setGroup(148)
 RingbitCar.init_wheel(AnalogPin.P0, AnalogPin.P1)
 basic.forever(function () {
 	
